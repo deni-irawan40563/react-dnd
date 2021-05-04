e9540b072011b8141c11e05fd1cc020ed4116e8c
Deni Irawan Nugraha <deni.irawan40563@gmail.com>
2021-05-04 08:23:42 +0700

    minor : create basic router configuration

--- src/configs/routes/index.js
+++ src/configs/routes/index.js
@@ -1,9 +1,24 @@
+// Import React Dependency
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+//Import Components
+import HomePage from '../../containers/pages/HomePage/HomePage'
+import LoginPage from '../../containers/pages/LoginPage/LoginPage'
+import RegisterPage from '../../containers/pages/RegisterPage/RegisterPage'
 
 function route() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route component={HomePage} path="/" exact/>
+        <Route component={LoginPage} path="/login" exact/>
+        <Route component={RegisterPage} path="/register" exact/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/containers/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RegisterPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This Register Page</h1>
+      </div>
+    )
+  }
+}
